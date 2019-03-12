84a0e78787ed68b992c575a32502948b7480d294
Govind Jeevan <govindjeevan7@gmail.com>
2019-03-13 00:53:29 +0530

    Asynchronous Get Requests

---
+++ r1.py
@@ -0,0 +1,28 @@
+import asyncio
+import requests
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+async def main():
+    loop = asyncio.get_event_loop()
+    futures = [
+        loop.run_in_executor(
+            None, 
+            requests.get, 
+            'http://example.org/'
+        )
+        for i in range(200)
+    ]
+    x=0
+    c=0
+    for response in await asyncio.gather(*futures):
+        x=x+response.elapsed.total_seconds()
+        c=c+1
+    print(x/c)
+    
+
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(main())
