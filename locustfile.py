5080bd54fafedd6807551341bd5c2ccd600405c9
smarty1palak <smarty1palak@gmail.com>
2019-03-20 17:40:13 +0530

    store the maximum no of clients a url can handle before failing

--- locustfile.py
+++ locustfile.py
@@ -1,4 +1,9 @@
 from locust import HttpLocust, TaskSet, task
+from locust import events
+from additional_handlers import additional_success_handler, additional_failure_handler
+
+# events.request_success += additional_success_handler
+# events.request_failure += additional_failure_handler
 
 class UserBehavior(TaskSet):
     def on_start(self):
@@ -7,13 +12,14 @@ class UserBehavior(TaskSet):
     def on_stop(self):
         """ on_stop is called when the TaskSet is stopping """
 
-
     @task(1)
     def index(self):
         self.client.get("/")
+
 
 
 class WebsiteUser(HttpLocust):
     task_set = UserBehavior
     min_wait = 5000
     max_wait = 9000
+
--- try.sh
+++ try.sh
@@ -2,8 +2,18 @@ while read url
 do
 	for i in {1..10}
 	do
-		let a="50*i+100"
-		locust --host=http://$url --no-web -c $a -r 100 --run-time 1m --csv=ex$url$i   
+		let a="50*i"
+		locust --host=http://$url --no-web -c $a -r 100 --run-time 1m --csv=ex$url$i  
+		echo "ex${url}${i}_requests.csv"
+		cat "ex${url}${i}_requests.csv"| awk -v FS=',' '{print $4}'| head -3 | tail -n 1
+		if [ "$(cat "ex${url}${i}_requests.csv"| awk -v FS=',' '{print $4}'| head -3 | tail -n 1)" -gt 40 ]
+		then
+			echo "maximum number of clients reached: $a"
+			echo >> analysis.txt
+			echo "${url} : ${a} " >> analysis.txt
+			pwd
+			break
+		fi
 	done
 	echo "$url" 
 done < $1
